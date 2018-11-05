adf9cc20f4ba3e194b919ce29ce39b5326b05a5a
yowe <ruwangxiansheng@sina.com>
2018-11-05 09:22:57 +0800

    clear

--- ClientUI/MainWindow.xaml.cs
+++ ClientUI/MainWindow.xaml.cs
@@ -19,9 +19,39 @@ namespace ClientUI
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        Other frm;
         public MainWindow()
         {
             InitializeComponent();
+
+            List<Student> list = new List<Student>() {
+                new Student{ Name="123"},
+                new Student{ Name="Tom"}
+            };
+            this.list1.DataContext = list;
         }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string path = @"E:\其他\需求问题\需求问题\20180725-20180731-佛山市南海中美玩具厂.xlsx";
+            ExcelReaderLib.ExcelReader reader = new ExcelReaderLib.ExcelReader(path);
+            reader.Reader();
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+
+    public class Student
+    {
+        public string Name { get; set; }
     }
 }
